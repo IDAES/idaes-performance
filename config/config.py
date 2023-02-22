30061e82fe5e26fd4bd5aeb8db9111838332563b
Miranda Mundt <mmundt@sandia.gov>
2023-02-22 08:50:34 -0700

    Add config files for webpage generation

---
+++ config/config.py
@@ -0,0 +1,11 @@
+"""
+Configuration details for IDAES performance plots
+"""
+
+CONFIG = {
+    'project': 'idaes',
+    'x-axis': 'build',
+    'y-axis': 'measure',
+    'fix': {},
+    'save_to_file': True
+    }
---
+++ config/index_config.py
@@ -0,0 +1,20 @@
+"""
+Configuration for index page for IDAES performance plots
+"""
+
+CONFIG = {
+    'project' : 'IDAES',
+    'project-url' : 'https://idaes.org/',
+    'repository-url' : 'https://github.com/IDAES/idaes-pse',
+    'menu-bar-urls' : [('Documentation', 'https://idaes-pse.readthedocs.io/en/stable/'),
+                       ('GitHub', 'https://github.com/IDAES/idaes-pse')],
+    'icon-url' : 'https://github.com/IDAES/examples-pse/blob/main/docs/_images/idaes-logo-100x100.png',
+    'hero-image-url' : 'https://github.com/IDAES/examples-pse/blob/main/docs/_static/idaes-logo-withtext.png',
+    'copyright-statement' : 
+        """
+              <a href="https://idaes.org" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1" target="_blank">
+                <img src="https://github.com/IDAES/examples-pse/blob/main/docs/_images/idaes-logo-100x100.png" style="width:42px;height:42px;">
+              </a>
+              <span class="mb-3 mb-md-0 text-muted">&copy; 2023 Institute for the Design of Advanced Energy Systems</span>
+        """
+    }
