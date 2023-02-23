06b86370e4cbe2107fa6216168dfa12ebb849f53
Miranda Mundt <mmundt@sandia.gov>
2023-02-23 09:38:45 -0700

    Fix links for images

--- config/index_config.py
+++ config/index_config.py
@@ -8,12 +8,12 @@ CONFIG = {
     'repository-url' : 'https://github.com/IDAES/idaes-pse',
     'menu-bar-urls' : [('Documentation', 'https://idaes-pse.readthedocs.io/en/stable/'),
                        ('GitHub', 'https://github.com/IDAES/idaes-pse')],
-    'icon-url' : 'https://github.com/IDAES/examples-pse/blob/main/docs/_images/idaes-logo-100x100.png',
-    'hero-image-url' : 'https://github.com/IDAES/examples-pse/blob/main/docs/_static/idaes-logo-withtext.png',
+    'icon-url' : 'https://raw.githubusercontent.com/IDAES/examples-pse/main/docs/_images/idaes-logo-100x100.png',
+    'hero-image-url' : 'https://raw.githubusercontent.com/IDAES/examples-pse/main/docs/_static/idaes-logo-withtext.png',
     'copyright-statement' : 
         """
               <a href="https://idaes.org" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1" target="_blank">
-                <img src="https://github.com/IDAES/examples-pse/blob/main/docs/_images/idaes-logo-100x100.png" style="width:42px;height:42px;">
+                <img src="https://raw.githubusercontent.com/IDAES/examples-pse/main/docs/_images/idaes-logo-100x100.png" style="width:42px;height:42px;">
               </a>
               <span class="mb-3 mb-md-0 text-muted">&copy; 2023 Institute for the Design of Advanced Energy Systems</span>
         """
